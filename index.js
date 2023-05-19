24698dba02177b084edc6c90a2be43a97cfed653
PangFive <t.wahyu96@gmail.com>
2023-05-20 04:05:57 +0700

    fix proxy

--- index.js
+++ index.js
@@ -35,56 +35,47 @@ fastify.get("/ippublic", async (req, res) => {
 });
 
 fastify.get("/proxy", async function (req, res) {
-  //   let url = decodeURIComponent(qs.stringify(req.query));
-  //   if (!url.includes("?") && url.slice(-1) == "=") {
-  //     url = url.replace("=", "");
-  //   }
+  let url = decodeURIComponent(qs.stringify(req.query));
+  if (!url.includes("?") && url.slice(-1) == "=") {
+    url = url.replace("=", "");
+  }
 
-  //   let config = {
-  //     headers: {
-  //       "User-Agent": "okhttp/3.14.9",
-  //     },
-  //   };
+  let config = {
+    headers: {
+      "User-Agent": "okhttp/3.14.9",
+    },
+  };
 
-  //   if (req.headers["content-type"] != undefined) {
-  //     config.headers["Content-Type"] = req.headers["content-type"];
-  //   }
+  if (req.headers["content-type"] != undefined) {
+    config.headers["Content-Type"] = req.headers["content-type"];
+  }
 
-  //   if (req.headers["authorization"] != undefined) {
-  //     config.headers["Authorization"] = req.headers["authorization"];
-  //   }
+  if (req.headers["authorization"] != undefined) {
+    config.headers["Authorization"] = req.headers["authorization"];
+  }
 
-  //   if (req.headers["x-client-id"] != undefined) {
-  //     config.headers["x-client-id"] = req.headers["x-client-id"];
-  //   }
+  if (req.headers["x-client-id"] != undefined) {
+    config.headers["x-client-id"] = req.headers["x-client-id"];
+  }
 
-  //   if (req.headers["x-api-key"] != undefined) {
-  //     config.headers["x-api-key"] = req.headers["x-api-key"];
-  //   }
+  if (req.headers["x-api-key"] != undefined) {
+    config.headers["x-api-key"] = req.headers["x-api-key"];
+  }
 
-  //   try {
-  //     await axios.get(url, config).then((response) => {
-  //       res.statusCode = response.status;
-  //       let data = response.data;
-  //       res.send({ ...data });
-  //     });
-  //   } catch (err) {
-  //     res.statusCode = err.response.status;
-  //     res.send({
-  //       status: "error",
-  //       message: err.message,
-  //       data: err?.data,
-  //     });
-  //   }
-  const options = { method: "GET" };
-
-  await fetch(
-    "https://map.bpkp.go.id/api/v6/presensi?niplama=201800378&tanggal_awal=2023-05-10&api_token=t1e2KhzoIed59PBnmGf7rli6EQlvjUOMvDvOMIpj0nB94YOoT9WKk5oBgsxjrwMh",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => res.send({ response }))
-    .catch((err) => console.error(err));
+  try {
+    await axios.get(url, config).then((response) => {
+      res.statusCode = response.status;
+      let data = response.data;
+      res.send({ ...data });
+    });
+  } catch (err) {
+    res.statusCode = err.response.status;
+    res.send({
+      status: "error",
+      message: err.message,
+      data: err?.data,
+    });
+  }
 });
 
 fastify.post("/proxy", async (req, res) => {
